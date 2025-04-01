633ce44300a7519fc257420bdf613954a478b0f7
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-04-01 20:04:40 -0300

    singleton .py

--- DESIGN PATTERNS/CRIACIONAL - creational/singleton.py
+++ DESIGN PATTERNS/CRIACIONAL - creational/singleton.py
@@ -1,7 +1,22 @@
 class Singleton:
     _instance = None
-    
+
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
+            print("Instância criada!")
+            cls._instance = super().__new__(cls)
         return cls._instance
+
+    def show_message(self):
+        print("Olá do Singleton!")
+
+# Testando o Singleton
+singleton = Singleton()
+singleton.show_message()
+
+# Tentando criar outra instância (deve ser a mesma)
+another_singleton = Singleton()
+another_singleton.show_message()
+
+if singleton is another_singleton:
+    print("As duas instâncias são a mesma!")
